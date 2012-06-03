762620d0b803a39e585ea961f8d0d6c70e0672f0
Nathan Broadbent <nathan.f77@gmail.com>
2012-06-03 15:46:41 +1200

    Added #url to issue trackers, for links on apps#index table

--- app/models/issue_trackers/github_issues_tracker.rb
+++ app/models/issue_trackers/github_issues_tracker.rb
@@ -32,5 +32,9 @@ class IssueTrackers::GithubIssuesTracker < IssueTracker
   def body_template
     @@body_template ||= ERB.new(File.read(Rails.root + "app/views/issue_trackers/github_issues_body.txt.erb").gsub(/^\s*/, ''))
   end
+
+  def url
+    "https://github.com/#{project_id}/issues"
+  end
 end
 
