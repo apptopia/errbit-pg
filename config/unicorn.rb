d8acf425f6d448ce03c2f27d5b17d09f7651daaf
Alex Naumenko <alecnmk@gmail.com>
2013-11-30 03:26:01 +0200

    app path for unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
 # http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+app_path = File.expand_path("../../..", __FILE__)
 
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
